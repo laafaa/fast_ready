86c16067e92fd748eea740044400a58753ade020
laafaa <lafa.crp@gmail.com>
2016-02-16 23:10:38 +0500

    ага

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   has_many :users_roles
   has_many :roles, :through => :users_roles
   has_and_belongs_to_many :games
-        validates :login, :phone_number, presence: true
-        validates :phone_number, length: { is: 11, message: " должен содержать 11 цифр"}
-        validates :phone_number, numericality: { only_integer: true, message: " должен содержать только цифры" }
-        validates :phone_number, :login, uniqueness: { is: true, message: " уже используется"}
+        validates :login, :phone, :email, :password, presence: true
+        validates :phone, length: { is: 11, message: " должен содержать 11 цифр"}
+        validates :phone, numericality: { only_integer: true, message: " должен содержать только цифры" }
+        validates :phone, :login, uniqueness: { is: true, message: " уже используется"}
 end

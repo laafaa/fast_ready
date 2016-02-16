0b6e6d714a609e8fce12896458c237316acd88a3
Upidev <upidev@gmail.com>
2016-02-16 07:02:49 -0800

    change routes GLAVNAYA

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

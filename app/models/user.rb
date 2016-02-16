4152bf4db7d2d0d8529fcbcd5a41840338889b18
laafaa <lafa.crp@gmail.com>
2016-02-16 21:52:44 +0500

    cancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :users_roles
+  has_many :roles, :through => :users_roles
 end

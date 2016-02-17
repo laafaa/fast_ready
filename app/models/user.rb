dd4b558de31c91ca9d08d4fa021153aec2ce42f4
laafaa <lafa.crp@gmail.com>
2016-02-17 19:27:50 +0500

    Отладка регистрации

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,13 @@ class User < ActiveRecord::Base
   has_many :roles, :through => :users_roles
   has_many :user_transactions
   has_and_belongs_to_many :games
+  
   validates :login, :phone, :email, :password, presence: true
   
-  validates :phone, length: { is: 11, message: " должен содержать 11 цифр"}
-  validates :phone, numericality: { only_integer: true, message: " должен содержать только цифры" }
-  validates :phone, :login, uniqueness: { is: true, message: " уже используется"}
-
+  validates :phone, length: { is: 11, 
+    message: " должен содержать 11 цифр"}
+  validates :phone, numericality: { only_integer: true, 
+    message: " должен содержать только цифры" }
+  validates :phone, :login, :email, uniqueness: { is: true, 
+    message: " уже используется"}
 end
